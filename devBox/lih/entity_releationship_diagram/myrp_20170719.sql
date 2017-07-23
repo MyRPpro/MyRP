15fcbe1158419edf465c89aa2ca9973fbaa63ae2
InhongLee <amaco78@gmail.com>
2017-07-23 15:37:52 +0900

    mid_save_db_tuning

--- devBox/lih/entity_releationship_diagram/myrp_20170719.sql
+++ devBox/lih/entity_releationship_diagram/myrp_20170719.sql
@@ -436,6 +436,52 @@ CREATE TABLE salary_register_statement (
     CONSTRAINT salary_register_statement_fk2 FOREIGN KEY(salary_register_id,account_id) REFERENCES salary_register(salary_register_id,account_id)
 );
 --------------------------------------------------------------------------------
+-- table state
+--------------------------------------------------------------------------------
+DROP TABLE STATE;
+CREATE TABLE STATE(
+  CODE        NUMBER(5),
+  KOR_NAME    VARCHAR(30), 
+  ENG_NAME    VARCHAR(40),
+  DEPT_NAME   VARCHAR(9),
+  ACCESS_ROLE VARCHAR(3)
+);
+ALTER TABLE STATE DROP COLUMN DEPT_NAME;
+ALTER TABLE STATE DROP COLUMN ACCESS_ROLE;
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (25451,'전표미승인','disapproval_statement','회계','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (25452,'전표승인','approval_statement','회계','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (25453,'전표승인거절','denial_statement','인사','HR');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (26451,'급여지급요청','request_payments_salary','인사','HR');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (26452,'급여지급대기','wait_payments_salary','인사','HR');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (26453,'급여지급완료','complete_payments_salary','인사','HR');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24101,'입고요청접수','receive_request_storage_in_warehouse','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24102,'입고완료','complete_storage_in_warehouse','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24201,'출고요청접수','receive_request_storage_out_warehouse','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24202,'출고대기','wait_storage_out_warehouse','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24203,'재고준비중','prepare_inventory','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24752,'출고완료','complete_storage_out_warehouse','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24753,'구매요청','request_purchase','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (24621,'재고조사완료','complete_inventory','물류','FI');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22211,'판매견적요청','check_sales_estimate','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22212,'판매견적승인','approval_sales_estimate','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22213,'판매전표승인요청','request_approval_sales_statement','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22214,'판매전표승인완료','complete_approval_sales_statement','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22215,'마감','deadline','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22222,'영업출고요청','request_sales_storage_out_warehouse','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22223,'영업출고완료','complete_sales_storage_out_warehouse','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22411,'수금대기','wait_colleciton','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (22412,'수금완료','complete_colleciton','영업','SA');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23201,'주문확인','check_order','구매','PU');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23202,'구매전표승인요청','request_approval_purchase_statement','구매','PU');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23203,'구매전표승인완료','complete_approval_purchase_statement','구매','PU');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23204,'구매입고요청','request_purchase_storage_in_warehouse','구매','PU');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23205,'구매입고완료','complete_purchase_storage_in_warehouse','구매','PU');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23206,'지급대기','wait_payments_salary','구매','PU');
+Insert into STATE (CODE,KOR_NAME,ENG_NAME,DEPT_NAME,ACCESS_ROLE) values (23207,'지급완료','complete_payments_salary','구매','PU');
+
+COMMIT;
+
+--------------------------------------------------------------------------------
 -- 사원 목록 조회(employeeVO)
 --------------------------------------------------------------------------------
 SELECT  *
@@ -843,8 +889,213 @@ ALTER TABLE sales_order DROP CONSTRAINT SALES_ORDER_FK2;
 ALTER TABLE purchase_order DROP CONSTRAINT PURCHASE_ORDER_FK2;
 ALTER TABLE stock_order DROP CONSTRAINT STOCK_ORDER_FK1;
 
+--------------------------------------------------------------------------------
+-- ORDER_STATE TABLE TRIGER 연결
+--------------------------------------------------------------------------------
+--DROP TRIGGER trigger_sales_insert;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_sales_insert
+    AFTER INSERT
+    ON sales_order
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    INSERT INTO order_state
+    VALUES(:NEW.sales_id, :NEW.sales_state);
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL'; 
 
+--DROP TRIGGER trigger_sales_update;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_sales_update
+    AFTER UPDATE
+    ON sales_order
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    UPDATE  order_state
+    SET     order_state = :NEW.sales_state
+    WHERE   order_id = :NEW.sales_id;
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL'; 
 
+--DROP TRIGGER trigger_purchase_insert;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_purchase_insert
+    AFTER INSERT
+    ON purchase_order
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    INSERT INTO order_state
+    VALUES(:NEW.purchase_id, :NEW.purchase_state);
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL'; 
+
+--DROP TRIGGER trigger_purchase_update;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_purchase_update
+    AFTER UPDATE
+    ON purchase_order
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    UPDATE  order_state
+    SET     order_state = :NEW.purchase_state
+    WHERE   order_id = :NEW.purchase_id;
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
+
+--DROP TRIGGER trigger_salary_insert;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_salary_insert
+    AFTER INSERT
+    ON salary_register
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    INSERT INTO order_state
+    VALUES(:NEW.salary_register_id, :NEW.salary_state);
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
+
+--DROP TRIGGER trigger_salary_update;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_salary_update
+    AFTER UPDATE
+    ON salary_register
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    UPDATE  order_state
+    SET     order_state = :NEW.salary_state
+    WHERE   order_id = :NEW.salary_register_id;
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
+
+--DROP TRIGGER trigger_statement_insert;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_statement_insert
+    AFTER INSERT
+    ON statement
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    INSERT INTO order_state
+    VALUES(:NEW.statement_id, :NEW.approval_state);
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
+
+--DROP TRIGGER trigger_statement_update;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_statement_update
+    AFTER UPDATE
+    ON statement
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    UPDATE  order_state
+    SET     order_state = :NEW.approval_state
+    WHERE   order_id = :NEW.statement_id;
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
+
+--DROP TRIGGER trigger_stock_insert;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_stock_insert
+    AFTER INSERT
+    ON stock_order
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    INSERT INTO order_state
+    VALUES(:NEW.stock_order_id, :NEW.stock_state);
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
+
+--DROP TRIGGER trigger_stock_update;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+SET SERVEROUTPUT ON
+CREATE OR REPLACE TRIGGER trigger_stock_update
+    AFTER UPDATE
+    ON stock_order
+    FOR EACH ROW
+BEGIN
+    dbms_output.put_line('Trigger running');
+    UPDATE  order_state
+    SET     order_state = :NEW.stock_state
+    WHERE   order_id = :NEW.stock_order_id;
+    dbms_output.put_line('Trigger operation successed');
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERR CODE'||TO_CHAR(SQLCODE));
+            dbms_output.put_line('ERR MESSAGE'||SQLERRM);
+END;
+/
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:ALL';
 --------------------------------------------------------------------------------
 -- 테스트 코드 사용하지 마세요.!!!
 --------------------------------------------------------------------------------
@@ -878,3 +1129,11 @@ SELECT	Max(appointment_date)
 		FROM	personnel_appointment
 		GROUP BY employee_id
 		HAVING	employee_id = 6001;
+-- 관리자 계정 생성 코드
+INSERT INTO DEPARTMENT(DEPT_ID,DEPT_NAME,ACCESS_ROLE)
+VALUES(1000,'관리자','ADMIN');
+INSERT INTO EMPLOYEE(EMPLOYEE_ID,EMPLOYEE_NAME,DEPT_ID,HR_CODE_GROUP_RANK,RANK_CODE,RESIDENCE_REG_NO)
+VALUES(1001,'관리자',1000,2,500,'8011111111111');
+INSERT INTO EMPLOYEE_INFO(EMPLOYEE_ID,HOURLY_WAGE)
+VALUES(1001,10000);
+COMMIT;
